7146f8e64f6cd759ea2cdf2cbf17c752d78659a1
kol-oss <kolyakhozhajinov@gmail.com>
2025-05-29 23:37:46 +0300

    Add task core interface

---
+++ src/main/java/io/github/kol/oss/taskforge/core/ITask.java
@@ -0,0 +1,15 @@
+package io.github.kol.oss.taskforge.core;
+
+import io.github.kol.oss.taskforge.core.status.IStatus;
+
+public interface ITask<T> {
+    void start();
+
+    void cancel();
+
+    T getResult() throws Exception;
+
+    Exception getException();
+
+    IStatus getStatus();
+}
